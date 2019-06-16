d4544ffd8d83151feab2eea6016a5e4981de7b06
Tulika Baranwal <tulika.baranwal@oracle.com>
2019-06-17 02:20:22 +0530

    Added placeholder code, to be used if Picasso is not recommended

---
+++ PlaceSearcher/app/src/main/java/com/tulikab/placesearcher/utils/DownloadImageAsyncTask.java
@@ -0,0 +1,13 @@
+package com.tulikab.placesearcher.utils;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+
+//TODO:: Instead of Picasso can use this
+public class DownloadImageAsyncTask extends AsyncTask<String, Void, Bitmap> {
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        return null;
+    }
+}
